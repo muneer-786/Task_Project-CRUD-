a8e48f2bc3062af340cc349f7f2f0637422b4d11
muneer-786 <muneeralif@gmail.com>
2024-01-04 08:46:01 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Task_Project_CRUD_.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
